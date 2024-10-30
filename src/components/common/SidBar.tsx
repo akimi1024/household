7a63b925445a761e71457af3f10b98b6ad78a179
宮澤昭海 <>
2024-10-31 07:38:24 +0900

    不要なインポートの削除

--- src/components/common/SidBar.tsx
+++ src/components/common/SidBar.tsx
@@ -2,13 +2,11 @@ import React, { CSSProperties } from 'react'
 
 import Drawer from '@mui/material/Drawer';
 import Divider from '@mui/material/Divider';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import MailIcon from '@mui/icons-material/Mail';
 import HomeIcon from '@mui/icons-material/Home';
 import EqualizerIcon from '@mui/icons-material/Equalizer';
 import { Box, Toolbar } from '@mui/material';
