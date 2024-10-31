07c5cb5e84c027343f9f577324088201bf0b5543
宮澤昭海 <>
2024-11-01 06:57:04 +0900

    Homeのカード作成

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,25 @@
+import { Box } from '@mui/material'
 import React from 'react'
+import MonthlySummary from '../components/MonthlySummary'
+import Calender from '../components/Calender'
+import TransactionForm from '../components/TransactionForm'
+import TransactionMenu from '../components/TransactionMenu'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Box sx={{display: "Flex"}}>
+      {/* 左側のコンテンツ */}
+      <Box sx={{flexGrow: 1}}>
+        <MonthlySummary />
+        <Calender />
+      </Box>
+
+      {/* 右側のコンテンツ */}
+      <Box>
+        <TransactionMenu />
+        <TransactionForm />
+      </Box>
+    </Box>
   )
 }
 
