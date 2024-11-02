32329aefc640430be620c2c0bf82bde7dca454cb
宮澤昭海 <>
2024-11-02 09:00:37 +0900

    firestoreからデータを取得

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Home from './pages/Home';
@@ -8,8 +8,33 @@ import AppLayout from './components/layout/AppLayout';
 import  { theme } from './theme/theme'
 import { ThemeProvider } from '@emotion/react';
 import { CssBaseline } from '@mui/material';
+import { Transaction } from './types/index';
+import { collection, getDocs } from "firebase/firestore";
+import { db } from './firebase';
 
 function App() {
+  const [transactions,setTransactions] = useState<Transaction[]>([]);
+
+  useEffect(() => {
+    const fetchTransactions = async() => {
+      try {
+        const querySnapshot = await getDocs(collection(db, "Transactions"));
+
+        const transactionData = querySnapshot.docs.map((doc) => {
+          return {
+            ...doc.data(),
+            id: doc.id,
+          } as Transaction
+        });
+
+        setTransactions(transactionData)
+      }catch(err){
+
+      }
+    }
+
+    fetchTransactions();
+  }, [])
   return (
     <ThemeProvider theme={theme}>
     <CssBaseline />
