f1614ccf3c132e1b9cdbbb0f50c2ca0164c85f13
宮澤昭海 <>
2024-11-03 07:37:02 +0900

    fullcalenderを導入

--- src/components/Calender.tsx
+++ src/components/Calender.tsx
@@ -1,8 +1,39 @@
+import FullCalendar from '@fullcalendar/react'
 import React from 'react'
+import dayGridPlugin from "@fullcalendar/daygrid"
+import jaLocale from "@fullcalendar/core/locales/ja"
+import "../calender.css"
+import { EventContentArg } from '@fullcalendar/core'
 
 const Calender = () => {
+  const events = [
+    {title: "Meeting", start: new Date(), income: 300, expense: 200, balance: 100 }
+  ]
+
+  const renderEventContent = (eventInfo: EventContentArg) => {
+    return (
+      <div>
+        <div className='money' id="event-income">
+          {eventInfo.event.extendedProps.income}
+        </div>
+        <div className='money' id="event-expense">
+          {eventInfo.event.extendedProps.expense}
+        </div>
+        <div className='money' id="event-balance">
+          {eventInfo.event.extendedProps.balance}
+        </div>
+      </div>
+    )
+  }
+
   return (
-    <div>Calender</div>
+    <FullCalendar
+      locale={jaLocale}
+      plugins={[dayGridPlugin]}
+      initialView='dayGridMonth'
+      events={events}
+      eventContent={renderEventContent}
+    />
   )
 }
 
