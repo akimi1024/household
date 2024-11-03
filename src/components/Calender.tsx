16411f95c2d8e1b7d6fc518735bcfbd17a33689e
宮澤昭海 <>
2024-11-04 07:26:28 +0900

    日付ごとの収支計算処理を追加

--- src/components/Calender.tsx
+++ src/components/Calender.tsx
@@ -4,11 +4,19 @@ import dayGridPlugin from "@fullcalendar/daygrid"
 import jaLocale from "@fullcalendar/core/locales/ja"
 import "../calender.css"
 import { EventContentArg } from '@fullcalendar/core'
+import { Transaction } from '../types'
+import { calculateDailyBalances } from '../utils/financeCalculations'
 
-const Calender = () => {
+interface CalenderProps {
+  monthlyTransactions: Transaction[]
+}
+
+const Calender = ({monthlyTransactions}: CalenderProps) => {
   const events = [
     {title: "Meeting", start: new Date(), income: 300, expense: 200, balance: 100 }
   ]
+
+  const dailyBalances = calculateDailyBalances(monthlyTransactions)
 
   const renderEventContent = (eventInfo: EventContentArg) => {
     return (
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -16,7 +16,7 @@ const Home = ({monthlyTransactions}: HomeProps) => {
       {/* 左側のコンテンツ */}
       <Box sx={{flexGrow: 1}}>
         <MonthlySummary monthlyTransactions={monthlyTransactions}/>
-        <Calender />
+        <Calender monthlyTransactions={monthlyTransactions}/>
       </Box>
 
       {/* 右側のコンテンツ */}
