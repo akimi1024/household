8cdd5c90773952577183921c14289940cda5bf48
宮澤昭海 <>
2024-11-19 06:41:25 +0900

    react-chartjs-2導入

--- src/components/BarChart.tsx
+++ src/components/BarChart.tsx
@@ -1,9 +1,60 @@
 import React from 'react'
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+
 
 const BarChart = () => {
-  return (
-    <div>BarChart</div>
-  )
+
+
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top' as const,
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Bar Chart',
+      },
+    },
+  };
+
+  const labels = ['2024-01-01', '2024-01-02', '2024-01-03', '2024-01-04', '2024-01-05', '2024-01-06', '2024-01-07'];
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: '支出',
+        data: [100, 200, 300, 400, 500, 600, 700],
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      },
+      {
+        label: '収入',
+        data: [100, 150, 200, 250, 300, 350, 400],
+        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+      },
+    ],
+  };
+
+  return <Bar options={options} data={data} />;
 }
 
 export default BarChart
